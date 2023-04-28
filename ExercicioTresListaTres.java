c9ed68ab03d6ae5f139567548eea376748ccef67
Lucas Ferreira <129918519+LucasAndradeF@users.noreply.github.com>
2023-04-27 23:20:20 -0300

    Add correções lógicas no código
    Alteração realizada na lógica do exercício

--- ExercicioTresListaTres.java
+++ ExercicioTresListaTres.java
@@ -6,37 +6,52 @@ public class ExercicioTresListaTres {
         Scanner sc = new Scanner(System.in);
             String titulo = "Validação de Informações";
             System.out.println(titulo);
-            System.out.println("Informe o seu nome:");
-            String nome = sc.next();
+            String nome, sexo, estadoCivil;
+            int idade;
+            double salario;
+            
+            do { 
+                System.out.println("Informe o seu nome:");
+                nome = sc.next();
                 if (nome.length() < 3) {
-                System.out.println("Erro!");
+                    System.out.println("O nome precisar ter mais que 3 caracteres!");
                 }
-                    System.out.println("Informe a sua idade:");
-                    int idade = sc.nextInt();
-                        if ((idade > 0) && (idade < 150)) {
-                        } else {System.out.println("Erro!");
-                        }
-                            System.out.println("Informe o seu salário:");
-                            double salario = sc.nextDouble();
-                                if (salario > 0) {
-                                } else {System.out.println("Erro!");
-                                }
-                                    System.out.println("Informe o seu sexo:");   
-                                    String sexo = sc.next();
-                                        if (sexo.equals("m") || (sexo.equals("f"))) {
-                                        } else {
-                                        System.out.println("Erro!");
-                                        }        
-                                            System.out.println("Informe o seu Estado Civil:");   
-                                            String estadoCivil = sc.next();
-                                                if (estadoCivil.equals("s") || (estadoCivil.equals("c")) || (estadoCivil.equals("v")) || (estadoCivil.equals("d"))) {
-                                                } else {
-                                                System.out.println("Erro!");
-                                                }
-                                                    sc.close();
-    }
-}
-                                
+            } while (nome.length() < 3);
+                        
+            do { 
+                System.out.println("Informe a sua idade:");
+                idade = sc.nextInt();
+                if (idade <= 0 || idade >= 150) {
+                   System.out.println("A idade deve estar entre 0 e 150 anos!");
+                }   
+                } while (idade <= 0 || idade >= 150);
+
+            do {
+                System.out.println("Informe o seu salário:");
+                salario = sc.nextDouble();
+                if (salario < 0) {
+                System.out.println("O salário deve ser maior que R$ 0.");
+                }
+            } while (salario < 0);
+
+            do {
+                System.out.println("Informe o seu sexo com 'M' ou 'F': Utilize ('M' para Masculino) e ('F' para Feminino)");   
+                sexo = sc.next();
+                    if (!sexo.equalsIgnoreCase("M") && (!sexo.equalsIgnoreCase("F"))) {
+                    System.out.println("O sexo escolhido deve ser masculino ou feminino.");
+                    }
+                } while (!sexo.equalsIgnoreCase("M") && (!sexo.equalsIgnoreCase("F")));
+            
+            do {
+                System.out.println("Informe o seu Estado Civil: Utilize ('S' para Solteiro), ('C' para Casado), ('V' para Viuvo) e ('D' para Divorciado)");   
+                estadoCivil = sc.next();
+                if (!estadoCivil.equalsIgnoreCase("s") && (!estadoCivil.equalsIgnoreCase("c")) && (!estadoCivil.equalsIgnoreCase("v")) && (!estadoCivil.equalsIgnoreCase("d"))) {
+                System.out.println("Estado Civil incorreto, tente novamente.");
+                }
+            } while (!estadoCivil.equalsIgnoreCase("s") && (!estadoCivil.equalsIgnoreCase("c")) && (!estadoCivil.equalsIgnoreCase("v")) && (!estadoCivil.equalsIgnoreCase("d")));
+                sc.close();
+        }
+    }                              
         
 
         
