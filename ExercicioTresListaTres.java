3eb2b63baa67543b0a00312acb1398d8a90336c3
Lucas Ferreira <129918519+LucasAndradeF@users.noreply.github.com>
2023-04-29 20:15:44 -0300

    Add orientações do exercício no código
    Foram adicionadas as orientações para a realização do algoritmo.

--- ExercicioTresListaTres.java
+++ ExercicioTresListaTres.java
@@ -1,3 +1,10 @@
+// Faça um programa que leia e valide as seguintes informações:
+// Nome: maior que 3 caracteres;
+// Idade: entre 0 e 150;
+// Salário: maior que zero;
+// Sexo: 'f' ou 'm';
+// Estado Civil: 's', 'c', 'v', 'd';
+
 import java.util.Scanner;
 
 public class ExercicioTresListaTres {
