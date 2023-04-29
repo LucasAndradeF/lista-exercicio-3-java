7f9a4c3671fac1ea30646c88d8435b7e1bfabef5
Lucas Ferreira <129918519+LucasAndradeF@users.noreply.github.com>
2023-04-29 20:19:47 -0300

    Add orientações do exercício no código
    Foram adicionadas as orientações para a realização do algoritmo.

--- ExercicioSeisListaTres.java
+++ ExercicioSeisListaTres.java
@@ -1,3 +1,5 @@
+// Faça um programa que receba dois números inteiros e gere os números inteiros que estão no intervalo compreendido por eles. 
+
 import java.util.Scanner;
 
 public class ExercicioSeisListaTres {
